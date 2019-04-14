86c4f8ff752355ed3d37a3da6703013d21eb7fa1
Johan van Tent <tentvanjohan@hotmail.com>
2019-04-14 21:46:55 +0200

    verwerking datasets verbeterd en betere vertaling naar voorkeurnamen

--- data-raw/verwerken_TWN.R
+++ data-raw/verwerken_TWN.R
@@ -3,43 +3,11 @@ library(readxl)
 
 source("data-raw/helper_functions.R")
 
+# Zie helper_functions.R
 update_twn()
 
-# update_twn <- function() {
-#   url <- "http://sofus.ecosys.nl/Taxus/Downloads/Taxalists/TWNList.XLS"
-#   destfile <- "data-raw/twn_lijst.xls"
-#   curl::curl_download(url, destfile)
-#   shell.exec("D:/Johan/R/autecodata/data-raw/twn_lijst.xls")
-#   twn_lijst <- read_excel(destfile) # werkt uitsluitend als het gedownloade bestand een keer geopend is geweest
-#   
-#   use_data(twn_lijst, overwrite = TRUE)
-#   message(lubridate::today())
-#   message("Vergeet niet de datum in de TWN-documentatie te updaten")
-# }
-
-twn_voorkeur <- 
-  twn_lijst %>% 
-  select(taxonname, refername) %>% 
-  mutate(refername = if_else(is.na(refername), taxonname, refername)) %>% 
-  tibble::deframe() #%>% .[1:10]
 
 
-# twn_voorkeur_namen <- function(namen_orig) {
-#   
-#   twn_voorkeur <- twn_lijst %>% 
-#     select(taxonname, refername) %>% 
-#     mutate(refername = if_else(is.na(refername), taxonname, refername)) %>% 
-#     tibble::deframe() 
-#   
-#   namen_voorkeur <- unname(twn_voorkeur[namen_orig])
-#   
-#   if (!identical(namen_orig, namen_voorkeur)) {
-#     twn_voorkeur_namen(namen_voorkeur)
-#   }
-# 
-#   namen_voorkeur  
-#   
-# }
-
-x <- autecodata::wew_mafa_data$taxonnaam[1:10] 
-twn_voorkeur[twn_voorkeur[x]]
+volgorde_taxonlevels <- readr::read_csv2("data-raw/volgorde_taxonlevels.csv") %>% 
+  arrange(volgorde) %>% 
+  mutate(taxonlevel = factor(taxonlevel, levels = taxonlevel, ordered = TRUE))
